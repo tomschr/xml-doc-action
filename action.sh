80f33bae514bb4a86942438f5df5970651c517fe
Tom Schraitle <tom_schr@web.de>
2021-05-12 17:03:30 +0200

    Improve action.sh
    * Parse passed command line with getopt to get global options
    * Correct error handling of pipes with set -o pipefail
    * Use exec instead of eval
    * Set output logging
    * Create .code-workspace
    * Always execute final step in the test workflow file

--- action.sh
+++ action.sh
@@ -3,6 +3,25 @@
 __VERSION__="0.1"
 __AUTHOR__="Tom Schraitle"
 
+CLIARGS="$@"
+DOCCONF=""
+# CLIARRAY=
+
+# ---------
+# String contains a substring in another string
+#
+# source: https://stackoverflow.com/a/20460402
+function string_contain() { [ -z "$1" ] || { [ -z "${2##*$1*}" ] && [ -n "$2" ];};}
+
+# ---------
+# Verbose error handling
+#
+function exit_on_error {
+    echo -e "ERROR: ${1}" >&2
+    exit 250;
+}
+
+
 echo "::group::action.sh script"
 echo "Version $__VERSION__ by $__AUTHOR__"
 echo "::endgroup::"
@@ -22,9 +41,69 @@ for var in ACTIONS_CACHE_URL ACTIONS_RUNTIME_TOKEN ACTIONS_RUNTIME_URL \
 done
 echo "::endgroup::"
 
+# Taken from daps to parse the global options
+ARGS=$(getopt -o d:e:hm:v:: -l adocattr:,adocimgdir:,adocset,builddir:,config:,color:,colour:,commands,debug,docconfig:,dapsroot:,envfile:,fb_styleroot:,force,help,jobs:,main:,schema:,styleroot:,verbosity::,version,xsltprocessor: -n "action.sh" -- $CLIARGS)
+
+# Exit when getopt returns errors
+#
+GETOPT_RETURN_CODE=$?
+[[ 0 -ne $GETOPT_RETURN_CODE ]] && exit $GETOPT_RETURN_CODE
+
+eval set -- "$ARGS"
+
+while true ; do
+    case "$1" in
+        --builddir)
+            [[ -e $2 ]] || mkdir -p "$2"
+            BUILDDIR="$2"
+            shift 2
+            ;;
+
+        -d|--docconfig|-e|--envfile)
+            DOCCONF=$2
+            shift 2
+            ;;
+
+        # skip these options
+        --adocset|--commands|--debug|--force|-h|--help|--version)
+            shift
+            ;;
+
+        # skip these options
+        --adocattr|--adocimgdir|--config|--color|\
+        --colour|--dapsroot|--fb_styleroot|\
+        --jobs|-m|--main|--schema|--styleroot|-v|--verbosity|--xsltprocessor)
+            shift 2
+            ;;
+
+        --) shift ; break ;;
+        *) exit_on_error "Internal error!" ;;
+    esac
+done
+
+read -a CLIARRAY <<< $*
+
+if [[ ${CLIARRAY[0]} != "daps" ]]; then
+    exit_on_error "Command has to start with 'daps'"
+fi
+
+
+
 echo "::group::Calling $@"
-bash -c "$@"
+
+echo "Builddir=${BUILDDIR:=build}"
+echo "DC-FILE=${DOCCONF:=daps}"
+
+# We need to capture the exit status of the pipe:
+# https://stackoverflow.com/a/1221870
+(
+set -o pipefail
+mkdir -p ${BUILDDIR} 2>/dev/null || true
+exec $CLIARGS | tee ${BUILDDIR}/${DOCCONF}.log
+)
 RES=$?
 echo "::endgroup::"
 
 echo "::set-output name=dapsexitcode::$RES"
+echo "::set-output name=logfile::${BUILDDIR:-build}/$DOCCONF.log"
+exit $RES
